0be7fb3ba6182d843df9be65d54325aef7c29b5f
Jerome DAVID <programingjd@gmail.com>
2017-05-13 13:30:06 +0200

    -

--- src/main/java/info/jdavid/ok/server/handler/AcmeChallengeHandler.java
+++ src/main/java/info/jdavid/ok/server/handler/AcmeChallengeHandler.java
@@ -1,10 +1,14 @@
 package info.jdavid.ok.server.handler;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Map;
 
 import info.jdavid.ok.server.Response;
+import info.jdavid.ok.server.StatusLines;
 import okhttp3.HttpUrl;
+import okio.Okio;
 
 
 @SuppressWarnings({ "WeakerAccess" })
@@ -30,12 +34,31 @@ public class AcmeChallengeHandler implements Handler {
 
   @Override
   public String[] matches(final String method, final HttpUrl url) {
-    return new String[0];
+    final List<String> segments = url.pathSegments();
+    return segments.toArray(new String[segments.size()]);
   }
 
   @Override
   public Response.Builder handle(final Request request, final String[] params) {
-    return null;
+    //noinspection ConstantConditions
+    final File dir = directory == null ? domainDirectories.get(request.url.host()) : directory;
+    if (dir == null) {
+      return new Response.Builder().statusLine(StatusLines.NOT_FOUND).noBody();
+    }
+    File file = dir;
+    for (final String segment: params) {
+      file = new File(file, segment);
+      if (!file.exists()) return new Response.Builder().statusLine(StatusLines.NOT_FOUND).noBody();
+    }
+    if (!file.isFile()) return new Response.Builder().statusLine(StatusLines.NOT_FOUND).noBody();
+    try {
+      return new Response.Builder().
+        statusLine(StatusLines.OK).
+        body(Okio.buffer(Okio.source(file)), file.length());
+    }
+    catch (final FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
   }
 
 }
